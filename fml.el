2b708bd7e90a91316ae1dad0ca810baacacdbf49
tottoto <tottotodev@gmail.com>
2020-07-05 20:51:22 +0900

    Add header for emacs library

--- fml.el
+++ fml.el
@@ -1,3 +1,29 @@
+;;; fml.el --- Form markup language to generate HTML
+
+;; Copyright (C) 2020 tottoto
+
+;; Author: tottoto <tottotodev@gmail.com>
+;; Maintainer: tottoto <tottotodev@gmail.com>
+;; Keywords: lisp html
+;; Homepage: https://github.com/tottoto/fml.el
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Code:
+
 (defgroup fml nil
   "Form markup language to generate HTML."
   :group 'lisp
